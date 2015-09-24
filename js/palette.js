a6cc60a7d213fd411598411c1277bba40f921e84
chawlaaditya8 <chawlaaditya8@gmail.com>
2015-09-24 23:56:57 +0530

    Elevation
    Elevation-Step 1

--- js/palette.js
+++ js/palette.js
@@ -1,33 +1,3 @@
 $(".def").click(function(){
   $(".abc").css("background-color", $(this).css("background-color"));
 });
-// (function (window, $) {
-  
-//   $(function() {
-    
-    
-//     $('.def').on('click', function (event) {
-//       event.preventDefault();
-      
-//       var rippleObj = $(".box");
-//       var $div = $('<div/>');
-      
-//       $div.addClass('ripple-effect');
-//       var $ripple = $(".ripple-effect");
-      
-//       $ripple.css("height", rippleObj.height());
-//       $ripple.css("width", rippleObj.width());
-//       $div
-//         .css({
-//           background: rippleObj.data("ripple-color")
-//         }) 
-//         .appendTo(rippleObj);
-
-//       window.setTimeout(function(){
-//         $div.remove();
-//       }, 20);
-//     });
-    
-//   });
-  
-// })(window, jQuery);
---
+++ js/papershadow.js
@@ -0,0 +1,15 @@
+$(".one").click(function(){
+  $('.paper-card').attr( 'elevation','1');
+});
+$(".two").click(function(){
+  $('.paper-card').attr( 'elevation','2');
+});
+$(".three").click(function(){
+  $('.paper-card').attr( 'elevation','3');
+});
+$(".four").click(function(){
+  $('.paper-card').attr( 'elevation','4');
+});
+$(".five").click(function(){
+  $('.paper-card').attr( 'elevation','5');
+});
